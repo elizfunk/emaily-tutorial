05c008c6a6a5fafb1050262eae3797bd93f16c41
Elizabeth Funk <elizabeth_funk@hotmail.com>
2022-08-18 19:28:16 -0400

    update dev script; refactor passport.js

--- services/passport.js
+++ services/passport.js
@@ -17,22 +17,19 @@ passport.deserializeUser((id, done) => {
 })
 
 passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({googleId: profile.id})
-    .then(existingUser => {
-      console.log("existing user:", existingUser)
-      if (existingUser) {
-        // we already have a record with the given profile ID
-        done(null, existingUser)
-      } else {
-        // we don't have a user record with this ID, make one
-        new User({googleId: profile.id})
-          .save()
-          .then(user => done(null, user))
-      }
-    })
-})) 
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+  }, 
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({googleId: profile.id})
+
+    if (existingUser) {
+      return done(null, existingUser)
+    }
+
+    const user = await new User({googleId: profile.id}).save()
+    done(null, user)
+
+}))
