4a287ee31776424fcbcd029e9b3bd19635b830a0
Elizabeth Funk <elizabeth_funk@hotmail.com>
2022-08-23 19:43:07 -0400

    add stripe

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,10 @@
-export const authReducer = (state = {}, action) => {
+import { FETCH_USER } from "../actions/types"
+
+export const authReducer = (state = null, action) => {
+  console.log("action:", action)
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false
     default:
       return state
   }
